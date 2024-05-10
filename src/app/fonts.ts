191957baf2eaf7ab3a177ad72428e08b3e01b4e3
vsantos1711 <vsantos067100@gmail.com>
2024-05-10 14:05:31 -0300

    feat: Add font files and exports for Fira Code and Nunito

---
+++ src/app/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Fira_Code, Nunito } from "next/font/google";
+
+export const fira_code = Fira_Code({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const nunito = Nunito({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "800",
+});
