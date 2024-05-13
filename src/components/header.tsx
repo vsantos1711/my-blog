5009ea438abfa8e54cd5862c718e8ebd3061ccbd
vsantos1711 <vsantos067100@gmail.com>
2024-05-12 22:45:20 -0300

    refactor: Update header component to use lazy loading for images

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import { nunito } from "@/app/fonts";
+
 import GithubLogo from "./github-logo";
 import LinkedInLogo from "./linkedIn-logo";
 import CodesRankLogo from "./codesRank-logo";
-import { nunito } from "@/app/fonts";
 
-export function Header() {
+export default function Header() {
   return (
     <header className="min-h-20">
       <nav className="flex items-center gap-5 text-stone-400">
@@ -12,12 +13,12 @@ export function Header() {
           href="/"
           className={
             nunito.className +
-            " text-4xl hover:text-stone-100 transition focus:text-stone-100"
+            " text-4xl hover:text-stone-100 transition focus:text-stone-100 text-stone-100"
           }
         >
           vsantos
         </Link>
-        <ul className="flex space-x-4">
+        <ul className="flex gap-4">
           <li>
             <GithubLogo />
           </li>
