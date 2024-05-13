6040f3bccd10ea56c22f063a99ae77d17296e2ea
vsantos1711 <vsantos067100@gmail.com>
2024-05-13 20:04:48 -0300

    refactor: Update page component to include PostRoot and related components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,42 @@
 import Link from "next/link";
 
 import DynamicLink from "@/components/dynamicLink";
+import {
+  PostDate,
+  PostDescription,
+  PostReadTime,
+  PostRoot,
+  PostTags,
+  PostTimeRoot,
+  PostTitle,
+} from "@/components/post";
 
-import { words } from "@/consts/dynamicLinks";
+import { links } from "@/consts/dynamicLinks";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <section className="mb-5">
         <p>
-          Olá, meu nome é Vinícius! <DynamicLink words={words} />
+          Olá, meu nome é Vinícius! <DynamicLink links={links} />
           <br />
           <span>
             Se você é um recrutador, dê uma olhada no meu{" "}
-            <Link href="/cv" className="hover:underline">
+            <Link href="about/cv" className="hover:underline text-sky-500">
               currículo.
             </Link>
           </span>
         </p>
       </section>
+      <PostRoot>
+        <PostTitle title="Título do post" slug="post1" />
+        <PostTags tags={["tag1", "tag2", "tag3"]} />
+        <PostTimeRoot>
+          <PostDate date="2021-09-01T08:30:00Z" />
+          <PostReadTime readTime="5 min" />
+        </PostTimeRoot>
+        <PostDescription description="Descrição do post" />
+      </PostRoot>
     </main>
   );
 }
