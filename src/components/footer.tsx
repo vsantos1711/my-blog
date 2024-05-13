88287a52500444c508dac00df010ef3031e23bea
vsantos1711 <vsantos067100@gmail.com>
2024-05-12 22:43:22 -0300

    feat: Create Footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer>
+      <nav className="flex items-center gap-5 text-stone-400">
+        <ul className="flex space-x-4">
+          <li>github</li>
+          <span>•</span>
+          <li>linkedIn</li>
+          <span>•</span>
+          <li>resume</li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
