c44b80f5f9ad44cc0a6b6f01934fc5bb261f6972
vsantos1711 <vsantos067100@gmail.com>
2024-05-12 22:45:30 -0300

    feat: Add dynamicLinks constant with words and links

---
+++ src/consts/dynamicLinks.ts
@@ -0,0 +1,20 @@
+export const words = [
+  {
+    word: "Eu sou um desenvolvedor fullstack",
+    link: "/about",
+    target: "_blank",
+  },
+  {
+    word: "Construo aplicações escaláveis e performáticas",
+    link: "https://github.com/vsantos1711",
+    target: "_blank",
+  },
+  {
+    word: "Minha principal stack é o javascript",
+    link: "/projects/javascript",
+  },
+  {
+    word: "Atualmente estou interessado em Serverless Apps",
+    link: "/projects/serverless",
+  },
+];
