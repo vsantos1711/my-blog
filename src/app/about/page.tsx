4fa7d03a8f1954020ac74bee9f78d12b9c33a9f3
vsantos1711 <vsantos067100@gmail.com>
2024-05-13 20:03:49 -0300

    chore: create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <main className="min-h-screen">
+      <section className="mb-5">
+        <p>Olá, aqui será a tela Sobre Mim!</p>
+      </section>
+    </main>
+  );
+}
