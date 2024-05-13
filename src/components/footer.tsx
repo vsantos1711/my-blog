fb091dc14982f9867de0edafddad1eb2bb4b858c
vsantos1711 <vsantos067100@gmail.com>
2024-05-12 22:56:18 -0300

    refactor: Update footer component to use Next.js Link component for better navigation control

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,19 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer>
-      <nav className="flex items-center gap-5 text-stone-400">
-        <ul className="flex space-x-4">
-          <li>github</li>
+      <nav className="flex items-center text-stone-400">
+        <ul className="flex gap-2">
+          <Link href="https://github.com/vsantos1711" target="_blank">
+            github
+          </Link>
           <span>•</span>
-          <li>linkedIn</li>
+          <Link href="https://www.linkedin.com/in/vsantos1711/" target="_blank">
+            linkedIn
+          </Link>
           <span>•</span>
-          <li>resume</li>
+          <Link href="/cv">resume</Link>
         </ul>
       </nav>
     </footer>
