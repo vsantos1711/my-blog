7efc020d2279d24174d1c8a38a2bd6ca31052420
vsantos1711 <vsantos067100@gmail.com>
2024-05-13 20:04:10 -0300

    chore: create tags page

---
+++ src/app/tags/page.tsx
@@ -0,0 +1,13 @@
+export default function Tags() {
+  return (
+    <main className="min-h-screen">
+      <section className="mb-5">
+        <p>Olá, aqui será a tela de Tags!</p>
+        <span>
+          vou listar todas as tags navegando para <code> tags/javascript</code>.
+          Isso vai mostrar todos os projetos que tem o topic Javascript
+        </span>
+      </section>
+    </main>
+  );
+}
