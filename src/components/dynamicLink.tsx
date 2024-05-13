da5055a6f7460137ec5b8f6aa466769e059ba3b0
vsantos1711 <vsantos067100@gmail.com>
2024-05-12 22:45:41 -0300

    feat: Add DynamicLink component for displaying dynamic links with animations

---
+++ src/components/dynamicLink.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+
+import { variants } from "@/consts/animationVariants";
+
+interface Word {
+  word: string;
+  link: string;
+  target?: string;
+}
+
+export default function DynamicLink({ words }: { words: Word[] }) {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [prevIndex, setPrevIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setPrevIndex(currentIndex);
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 2500);
+
+    return () => clearInterval(intervalId);
+  }, [words.length, currentIndex]);
+
+  return (
+    <>
+      {words.map((word, index) => {
+        const animation =
+          index === currentIndex
+            ? "active"
+            : index === prevIndex
+            ? "previous"
+            : "other";
+
+        return (
+          <Link href={word.link} key={index} target={word.target}>
+            <motion.span
+              className="hover:underline px-1 absolute"
+              initial={{ opacity: 0, y: 20 }}
+              animate={variants[animation]}
+              transition={{ duration: 0.3 }}
+            >
+              {word.word}.
+            </motion.span>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
