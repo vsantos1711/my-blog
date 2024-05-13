21f0dcaf81c587f1a2e3ba3cfe7d29654a29ce88
vsantos1711 <vsantos067100@gmail.com>
2024-05-13 20:04:00 -0300

    chore: create cv page

---
+++ src/app/about/cv/page.tsx
@@ -0,0 +1,13 @@
+export default function CV() {
+  return (
+    <main className="min-h-screen">
+      <section className="mb-5">
+        <p>
+          Olá, meu nome é Vinícius!
+          <br />
+          <span>Aqui você pode vai ver o meu CURRÍCULO</span>
+        </p>
+      </section>
+    </main>
+  );
+}
