b29ae09d762d971d55afdcfddc4a1e0af83cfac4
vsantos1711 <vsantos067100@gmail.com>
2024-05-15 16:05:20 -0300

    refactor: Add countTopics function to calculate topic counts for repositories

---
+++ src/utils/topicCounter.ts
@@ -0,0 +1,22 @@
+import { IRepository } from "@/types";
+
+const ignoredTopics = ["useful", "utilities", "readme-template", "studies"];
+
+export function countTopics(
+  repositories: IRepository[]
+): { topic: string; count: number }[] {
+  const topicCounts: { [topic: string]: number } = {};
+
+  repositories.forEach((repo) => {
+    repo.topics.forEach((topic) => {
+      topicCounts[topic] = (topicCounts[topic] || 0) + 1;
+    });
+  });
+
+  const topicArray = Object.keys(topicCounts).map((topic) => ({
+    topic,
+    count: topicCounts[topic],
+  }));
+
+  return topicArray.filter((topic) => !ignoredTopics.includes(topic.topic));
+}
