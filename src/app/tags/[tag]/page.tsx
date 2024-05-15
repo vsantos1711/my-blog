60b800a76b137427a63816183b4e5149e5efc511
vsantos1711 <vsantos067100@gmail.com>
2024-05-15 15:01:12 -0300

    refactor: Update Tag page component to fetch and display repositories by tag

--- src/app/tags/[tag]/page.tsx
+++ src/app/tags/[tag]/page.tsx
@@ -1,9 +1,36 @@
-export default function Tag({ params }: { params: { slug: string } }) {
+import Link from "next/link";
+
+import { IRepository } from "@/types";
+import { filterRepoByTag } from "@/utils/filterRepo";
+
+export default async function Tag({ params }: { params: { tag: string } }) {
+  const data = await fetch(
+    "https://api.github.com/users/vsantos1711/repos"
+  ).then((res) => res.json());
+
+  const listOfRepos: IRepository[] = filterRepoByTag(data, params.tag);
+
   return (
     <main className="min-h-screen">
-      <div>
-        Aqui vai mostrar todos os posts com o seguinte tópico: {params.slug}
-      </div>
+      <h1 className="text-2xl font-extrabold mb-3 max-w-fit">
+        Total de post com a tag{" "}
+        <span className="text-sky-500">{`"${params.tag}"`}</span>:{" "}
+        {listOfRepos.length}
+      </h1>
+      <ul className="mb-5 pl-2">
+        {listOfRepos.map((repo: IRepository) => {
+          return (
+            <li key={repo.name}>
+              <Link href={`/project/${repo.name}`}>
+                • <span className="hover:underline text-lg">{repo.name}</span>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <Link href="/tags" className="hover:underline">
+        Todas as tags
+      </Link>
     </main>
   );
 }
