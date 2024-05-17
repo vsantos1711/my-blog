bfef53debc210fc890293803789bd8624aea65de
vsantos1711 <vsantos067100@gmail.com>
2024-05-17 09:22:04 -0300

    chore: Add GitHub markdown styles and update npm dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 
 import "../styles/globals.css";
+import "../styles/github-markdown.css";
 
 export const metadata: Metadata = {
   title: "My portfolio",
