c8d22c9dddd1bf52226fabc32d12dff21485c5cf
vsantos1711 <vsantos067100@gmail.com>
2024-06-21 13:56:00 -0300

    chore: Update page components to use Markdown for rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,20 @@ import {
 } from "@/components/post";
 import { links } from "@/utils/consts/dynamicLinks";
 import { filterRepo } from "@/utils/functions/filterRepo";
-import { IRepository, getRepositories } from "@/api";
+import { IRepository, getRepositories, getUserInfo } from "@/api";
+import { getRepoReadTime } from "@/utils/functions/getReadTime";
 
 export default async function Home() {
-  const data = await getRepositories();
-
-  const listOfRepos: IRepository[] = filterRepo(data);
-
+  const projects = await getRepositories();
+  const user = await getUserInfo();
+  const listOfProjects: IRepository[] = await filterRepo(projects);
   return (
     <main className="min-h-screen ">
       <section className="mb-14">
         <p>
           Hello! My name is{" "}
           <Link href="/about" className="hover:underline ">
-            Vinícius
+            {user.name}
           </Link>
           ! <DynamicLink links={links} />
           <br />
@@ -37,14 +37,14 @@ export default async function Home() {
           </span>
         </p>
       </section>
-      {listOfRepos.map((repo: IRepository) => {
+      {listOfProjects.map((repo: IRepository) => {
         return (
           <PostRoot key={repo.name}>
             <PostTitle title={repo.name} slug={repo.name} />
             <PostTags tags={repo.topics} />
             <PostTimeRoot>
               <PostDate date={repo.updated_at} />
-              <PostReadTime readTime="5 min" />
+              <PostReadTime readTime={getRepoReadTime(repo.name)} />
             </PostTimeRoot>
             <PostDescription description={repo.description} />
           </PostRoot>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,10 @@ import { nunito } from "@/app/fonts";
 import GithubLogo from "./github-logo";
 import LinkedInLogo from "./linkedIn-logo";
 import CodesRankLogo from "./codesRank-logo";
+import { getUserInfo } from "@/api";
 
-export default function Header() {
+export default async function Header() {
+  const user = await getUserInfo();
   return (
     <header className="min-h-20">
       <nav className="flex items-center gap-5 text-stone-400">
@@ -16,7 +18,7 @@ export default function Header() {
             " text-4xl hover:text-stone-100 transition focus:text-stone-100 text-stone-100"
           }
         >
-          vsantos
+          {user.login}
         </Link>
         <ul className="flex gap-4">
           <li>
